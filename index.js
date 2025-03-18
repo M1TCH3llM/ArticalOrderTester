d46098b5c2058d88033367249d317df88193b593
Mitchell Morgan <mitchellmorgan@Mitchells-MBP.localdomain>
2025-03-17 19:03:39 -0600

    updated file strucktur and modulized scrape

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
-import { convertTimeStamp } from './convertTimeStamp.js';
+import { convertTimeStamp } from './utils/convertTimestamp.js';
+import { scrapeArticles } from './utils/scrapeArticals.js';
 
 // Improts chromium browser objective from playwright library.
 // playwrite is a library that allows you to automate browsers.
@@ -7,59 +8,6 @@ import { convertTimeStamp } from './convertTimeStamp.js';
 import { chromium } from 'playwright';
 
 // This function scrapes articles from the Hacker News front page.
-async function scrapeArticles(page, articles, pageCount, outOfOrder) {
-  // log the page number that is being scraped
-    console.log(`Scraping page ${pageCount + 1}...`);
-
-    // wait for the page to load 
-    //.athing is the class of the article element
-    const newArticles = await page.$$eval('.athing', rows =>
-      // map through the rows and get the title and timestamp of each article
-        rows.map(row => {
-          // get the title of the article
-            const title = row.querySelector('.titleline a')?.innerText;
-            // get the timestamp of the article
-            const timeElement = row.nextElementSibling?.querySelector('.age');
-            // get the title and timestamp of the article
-            const timestamp = timeElement ? timeElement.getAttribute('title') : null;
-            // return the title and timestamp of the article
-            return { title, timestamp };
-            // filter out articles that do not have a timestamp
-        }).filter(article => article.timestamp)
-    );
-    // push the new articles to the articles array
-    articles.push(...newArticles);
-    // log the number of articles found on the page
-    console.log(`Found ${newArticles.length} articles on page ${pageCount}. Total articles: ${articles.length}`);
-      // Indicate to stop if 100 articles reached
-    if (articles.length >= 100) return true; 
-    // block handles potential errors when clicking the "More" button.
-    try {
-      // finds more button element
-        const moreButton = await page.$('a.morelink');
-        // checks if the more button was found
-        if (moreButton) {
-          //  clicks the more button
-            const nextUrl = await moreButton.getAttribute('href');
-            // checks if the nextUrl is not null
-            if (nextUrl) {
-                await page.goto(`https://news.ycombinator.com/${nextUrl}`);
-                await page.waitForLoadState('networkidle');
-                await page.waitForSelector('.athing');
-            }
-        } else {
-           // Indicate to stop if no more button
-            console.error("No 'More' button found.");
-            // return true;
-        }
-    } catch (error) {
-      // Indicate to stop on error
-        console.error("Error clicking 'More' button:", error);
-        // return true; 
-    }
-    // Indicate to continue
-    return false; 
-}
 // This function checks if the articles are in order from newest to oldest.
 function checkArticlesOrder(articles, testedArticles, lastTestedTimestamp, outOfOrder) {
   // loop through the articles
@@ -98,7 +46,7 @@ async function ArticalsInOrder() {
         // create a new page
         const page = await browser.newPage();
         // navigate to the hacker news website
-        await page.goto('https://news.ycombinator.com/');
+        await page.goto('https://news.ycombinator.com/newest');
 
         // initialize variables
         let articles = [];
---
+++ utils/scrapeArticals.js
@@ -0,0 +1,56 @@
+
+
+export async function scrapeArticles(page, articles, pageCount, outOfOrder) {
+    // log the page number that is being scraped
+      console.log(`Scraping page ${pageCount + 1}...`);
+  
+      // wait for the page to load 
+      //.athing is the class of the article element
+      const newArticles = await page.$$eval('.athing', rows =>
+        // map through the rows and get the title and timestamp of each article
+          rows.map(row => {
+            // get the title of the article
+              const title = row.querySelector('.titleline a')?.innerText;
+              // get the timestamp of the article
+              const timeElement = row.nextElementSibling?.querySelector('.age');
+              // get the title and timestamp of the article
+              const timestamp = timeElement ? timeElement.getAttribute('title') : null;
+              // return the title and timestamp of the article
+              return { title, timestamp };
+              // filter out articles that do not have a timestamp
+          }).filter(article => article.timestamp)
+      );
+      // push the new articles to the articles array
+      articles.push(...newArticles);
+      // log the number of articles found on the page
+      console.log(`Found ${newArticles.length} articles on page ${pageCount}. Total articles: ${articles.length}`);
+        // Indicate to stop if 100 articles reached
+      if (articles.length >= 100) return true; 
+      // block handles potential errors when clicking the "More" button.
+      try {
+        // finds more button element
+          const moreButton = await page.$('a.morelink');
+          // checks if the more button was found
+          if (moreButton) {
+            //  clicks the more button
+              const nextUrl = await moreButton.getAttribute('href');
+              // checks if the nextUrl is not null
+              if (nextUrl) {
+                  await page.goto(`https://news.ycombinator.com/${nextUrl}`);
+                  await page.waitForLoadState('networkidle');
+                  await page.waitForSelector('.athing');
+              }
+          } else {
+             // Indicate to stop if no more button
+              console.error("No 'More' button found.");
+              // return true;
+          }
+      } catch (error) {
+        // Indicate to stop on error
+          console.error("Error clicking 'More' button:", error);
+          // return true; 
+      }
+      // Indicate to continue
+      return false; 
+  }
+
